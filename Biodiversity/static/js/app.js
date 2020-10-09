6d32d64c2086889fc84c589520b240d700b2d623
tawnynichols <65970072+tawnynichols@users.noreply.github.com>
2020-10-09 15:46:57 -0700

    commit demo info

--- Biodiversity/static/js/app.js
+++ Biodiversity/static/js/app.js
@@ -1,7 +1,7 @@
 function getValues(id) {
 
     // Fetch the JSON data and console log it
-    d3.json("samples.json").then(function(importedData) {
+    d3.json("samples.json").then(importedData => {
         // console.log(importedData.names);
         //console.log(importedData)
         // console.log(importedData.samples.otu_ids)
@@ -15,20 +15,14 @@ function getValues(id) {
 
         // add text to lable for plot
         var get_ids = top10_ids.map(d => "OUT " + d)
-        console.log(get_ids)
-
+            // console.log(get_ids)
 
         // Use the map method with the arrow function to return all the filtered movie metascores.
         var values = importedData.samples[0].sample_values.slice(0, 10).reverse();
 
-        // Get to 10 values
-        var top10_values = values.slice(0, 10).reverse()
-        console.log(top10_values[0])
-
-
         // create labels
         var labels = importedData.samples[0].otu_labels.slice(0, 10);
-        console.log(labels)
+        // console.log(labels)
 
         // Create title
         var id_title = importedData.names[0]
@@ -60,11 +54,8 @@ function getValues(id) {
 }
 
 // // On change to the DOM, call getData()
-// d3.selectAll("#selDataset").on("change", getData);
-
-// Function called by DOM changes
 function getData(id) {
-    d3.json("sampe.json").then((data) => {
+    d3.json("samples.json").then((data) => {
         var demoData = data.metadata;
         console.log(demoData)
 
@@ -72,19 +63,20 @@ function getData(id) {
         var info = demoData.filter(d => d.id.toString() === id)[0];
 
         // select demographic data
-        var demoInfo = d3.select("#sample-metdata");
+        var demoInfo = d3.select("#sample-metadata");
 
         // get demographic data for the id and append to panel
-        Object.defineProperties(info).foreach((key) => {
-            demoInfo.append("h5").text(key[0].toUperCase() + ": " + key[1] + "\n")
-        })
-    })
+        Object.entries(info).forEach((key) => {
+            demoInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1]);
+        });
+    });
 
 }
 
-// Update the restyled plot's values
-function updatePlotly(newdata) {
-    Plotly.restyle("bar", "values", [newdata]);
+// create  function for change event
+function optionChanged(id) {
+    getValues(id);
+    getData(id);
 }
 
 
@@ -98,10 +90,12 @@ function init() {
 
         //get ids for dropdow
         data.names.forEach(function(name) {
-                dropdownMenu.append("option").text(name).property("value");
-            }
+            dropdownMenu.append("option").text(name).property("value");
+        })
 
-        )
+        // call functions to display plot
+        getValues(data.names[0]);
+        getData(data.names[0]);
 
     })
 }
